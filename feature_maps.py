7311e55349d3327fda9517a12afeb4cb90a1ad7e
Melanin08 <zda23b009@iitmz.ac.in>
2026-04-08 01:17:25 +0530

    Update feature_maps.py

--- feature_maps.py
+++ feature_maps.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import matplotlib.pyplot as plt
 from torchvision import transforms
@@ -6,27 +7,37 @@ from PIL import Image
 from models.classification import VGG11Classifier
 
 
-# LOAD MODEL 
+# CHECK PATHS FIRST
+print("Checkpoint exists:", os.path.exists("/kaggle/input/checkpoint/classifier.pth"))
+print("Image exists:", os.path.exists("/kaggle/input/data/oxford-iiit-pet/images/american_bulldog_71.jpg.jpeg"))
+
+
+# LOAD MODEL
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = VGG11Classifier()
-checkpoint = torch.load("checkpoints/classifier.pth", map_location=device)
-model.load_state_dict(checkpoint["state_dict"])
+checkpoint = torch.load("/kaggle/input/datasets/melaninayman08/all-checkpoint/classifier.pth", map_location=device)
+
+if isinstance(checkpoint, dict) and "state_dict" in checkpoint:
+    model.load_state_dict(checkpoint["state_dict"], strict=False)
+else:
+    model.load_state_dict(checkpoint, strict=False)
+
 model.to(device)
 model.eval()
 
 
-# LOAD IMAGE 
+# LOAD IMAGE
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
 
-img = Image.open("dog.jpg").convert("RGB")
+img = Image.open("/kaggle/input/datasets/melaninayman08/dog-file/american_bulldog_71.jpg.jpeg").convert("RGB")
 img = transform(img).unsqueeze(0).to(device)
 
 
-# HOOKS 
+# HOOKS
 first_layer_output = []
 last_layer_output = []
 
@@ -37,30 +48,35 @@ def hook_last(module, input, output):
     last_layer_output.append(output.detach().cpu())
 
 
-# Attach hooks
-model.encoder.block1[0].register_forward_hook(hook_first)
-model.encoder.block5[-1].register_forward_hook(hook_last)
+# ATTACH HOOKS
+h1 = model.encoder.block1.block[0].register_forward_hook(hook_first)
+h2 = model.encoder.block5.block[0].register_forward_hook(hook_last)
 
 
-# Forward pass
+# FORWARD PASS
 with torch.no_grad():
     _ = model(img)
 
+h1.remove()
+h2.remove()
+
 
 # PLOT FUNCTION
-def plot_feature_maps(feature_maps, title):
-    maps = feature_maps[0][0]  # batch=1
-    plt.figure(figsize=(10, 10))
+def plot_feature_maps(feature_maps, title, num_maps=16):
+    maps = feature_maps[0][0]
+    num_maps = min(num_maps, maps.shape[0])
 
-    for i in range(16):  # show 16 maps
-        plt.subplot(4, 4, i+1)
+    plt.figure(figsize=(10, 10))
+    for i in range(num_maps):
+        plt.subplot(4, 4, i + 1)
         plt.imshow(maps[i], cmap="gray")
         plt.axis("off")
 
     plt.suptitle(title)
+    plt.tight_layout()
     plt.show()
 
 
-# SHOW 
+# SHOW
 plot_feature_maps(first_layer_output, "First Layer Feature Maps")
 plot_feature_maps(last_layer_output, "Last Layer Feature Maps")
